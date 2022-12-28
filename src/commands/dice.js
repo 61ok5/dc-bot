7f3081c375c91a8fce0a139bbc33b569de0b248a
61ok5 <lok60674211@gmail.com>
2022-12-28 12:01:51 +0000

    dice roller subcommand added

--- src/commands/dice.js
+++ src/commands/dice.js
@@ -5,24 +5,29 @@ module.exports = {
         data: new SlashCommandBuilder()
                 .setName('dice')
                 .setDescription('Rolling a dice with specified notation')
-		.addStringOption(option =>
-			option.setName('notation')
+		.addSubcommand(subcommand =>
+			subcommand
+			.setName('roll')
 			.setDescription('Sth like 4d6, 2d10 + 1d4 or 8dF.2 - 3')
-			.setRequired(true)),
+			.addStringOption(option =>
+				option.setName('notation')
+				.setDescription('Sth like 4d6, 2d10 + 1d4 or 8dF.2 - 3')
+				.setRequired(true)))
+	     	.addSubcommand(subcommand =>
+			subcommand
+			.setName('help')
+			.setDescription('Get some help')),
         async execute(interaction) {
-		const notation = interaction.options.getString('notation').replace(/\s+/g, '')
-		// const regex = /(\d+)?d(\d+|F(\.\d)?)((?:\+|-)(\d+))?/gi;
-		// const match = notation.match(regex)
-    		// if (!match) {
-		// 	await interaction.reply('Please input valid notation')
-		// 	return
-		// }
-		try {
-	        	const roll = new DiceRoll(notation);
-		       	await interaction.reply(`${roll}`);
-		} catch (error) {
-			await interaction.reply(`Given notation **${notation}** and error found:\n`+error.message)
+	    	if(interaction.options.getSubcommand() == 'help')
+			await interaction.reply('Please check:\nhttps://dice-roller.github.io/documentation/guide/notation/');
+		else if(interaction.options.getSubcommand() == 'roll') {
+			const notation = interaction.options.getString('notation').replace(/\s+/g, '')
+			try {
+				const roll = new DiceRoll(notation);
+				await interaction.reply(`${roll}`);
+			} catch (error) {
+				await interaction.reply(`Given notation **${notation}** and error found:\n`+error.message)
+			}
 		}
 	},
 };
-
